7f63e57c14690169a04e62a3b2152983d3d51a87
sailirawnang <saili.rawnang@wohlig.com>
2018-12-17 14:02:37 +0530

    changes in brand page, quickview page

--- frontend/js/controllers/brands.js
+++ frontend/js/controllers/brands.js
@@ -9,16 +9,79 @@ myApp.controller('BrandsCtrl', function ($scope, myService, TemplateService, Pro
     //     $scope.banner = data.data.data;
 
     // });
-    myService.ctrlBanners("Brands", function (data) {
-        $scope.banner = data;
-    });
+    // myService.ctrlBanners("Brands", function (data) {
+    //     $scope.banner = data;
+    // });
 
-    ProductService.getBrands(function (data) {
-        if (data.data.data) {
-            $scope.brands = data.data.data;
-        } else {
-            $scope.message = "No Product Found";
-        }
-    })
+    // ProductService.getBrands(function (data) {
+    //     if (data.data.data) {
+    //         $scope.brands = data.data.data;
+    //     } else {
+    //         $scope.message = "No Product Found";
+    //     }
+    // })
+
+
+    $scope.featured = [{
+        productId: '1',
+        image: 'img/home/8.jpg',
+        price: '2899',
+        productName: 'Lines full sleeve shirt with rollup',
+        extLink: 'home'
+    }, {
+        productId: '2',
+        image: 'img/home/9.jpg',
+        price: '2899',
+        productName: 'Lines full sleeve shirt with rollup',
+        extLink: 'home'
+    }, {
+        productId: '3',
+        image: 'img/home/10.jpg',
+        price: '2899',
+        productName: 'Lines full sleeve shirt with rollup',
+        extLink: 'home'
+    }, {
+        productId: '4',
+        image: 'img/home/8.jpg',
+        price: '2899',
+        productName: 'Lines full sleeve shirt with rollup',
+        extLink: 'home'
+    }, ];
+
+    // <!--End of Featured-->
+
+    // <!--Start of Buy The Look-->
+
+    $scope.getthelook = {
+        id: '1',
+        majorImage: 'img/home/15.jpg',
+        title: 'lorem ipsum is dummy',
+        description: 'lorem ipsum is dummy It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset'
+    };
+    // <!--End of Buy The Look-->
+
+    // Start of Blog Section
+
+    $scope.brands = [{
+        logo: 'img/brands/dockers.png'
+    }, {
+        logo: 'img/brands/eagle.png'
+    }, {
+        logo: 'img/brands/ms-custom.png'
+    }, {
+        logo: 'img/brands/tony1.png'
+    }, {
+        logo: 'img/brands/ralph-custom.png'
+    }, {
+        logo: 'img/brands/ms.png'
+    }, {
+        logo: 'img/brands/ms-custom.png'
+    }, {
+        logo: 'img/brands/tony1.png'
+    }, {
+        logo: 'img/brands/ralph-custom.png'
+    }, {
+        logo: 'img/brands/ms.png'
+    }, ];
 
 });
